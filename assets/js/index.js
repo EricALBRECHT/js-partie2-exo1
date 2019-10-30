4435152af28d419b273e5a18b1a86f98573e6be6
ALBRECHT Eric <e.albrecht@laposte.net>
2019-10-30 17:57:20 +0100

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+var img = document.getElementById("image1");
+img.addEventListener("mouseover", function(event){document.getElementById("image1").src=`assets/images/image1_2.jpg`; })
+img.addEventListener("mouseout", function(event){document.getElementById("image1").src=`assets/images/image1.jpg`; })
+ 
